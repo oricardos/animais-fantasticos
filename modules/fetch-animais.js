1a015e455c4645409b68fc7280faf7da97139710
oricardos <ricardo.rs95@gmail.com>
2022-07-25 21:49:44 -0300

    feat(animais numero):  fetch animais

--- modules/fetch-animais.js
+++ modules/fetch-animais.js
@@ -1,23 +1,27 @@
+import initAnimaNumero from './anima-numero.js';
+
 export default function fetchAnimais(){
-    async function getData(){
-        const response = await fetch('../animaisapi.json');
+    async function getData(url){
+        const response = await fetch(url);
         const data = await response.json();
         const numerosGrid = document.querySelector('.numeros-grid');
-
+    
         data.forEach(animal => {
             const divAnimal = createAnimal(animal);
             numerosGrid.appendChild(divAnimal);
-        })
+        });
+    
+        initAnimaNumero();
     }
-
+    
     function createAnimal(animal){
         const div = document.createElement('div');
         div.classList.add('numero-animal');
         div.innerHTML = `<h3>${animal.specie}</h3>
         <span data-numero>${animal.total}</span>`;
-
+    
         return div;
     }
-
-    getData()
+    
+    getData('./animaisapi.json');    
 }
--- script.js
+++ script.js
@@ -6,7 +6,7 @@ import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
-import initAnimaNumero from './modules/anima-numero.js';
+
 import initFuncionamento from './modules/funcionamento.js';
 import fetchAnimais from './modules/fetch-animais.js';
 
@@ -18,6 +18,5 @@ initModal();
 initTooltip();
 initDropdownMenu();
 initMenuMobile();
-initAnimaNumero();
 initFuncionamento();
 fetchAnimais();
