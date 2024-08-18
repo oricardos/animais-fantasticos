f2078d740eeb11235b8590781b82220747a0649f
oricardos <oricardo.dev@gmail.com>
2024-08-17 23:07:33 -0300

    aulas iniciais

---
+++ js-iniciantes/tipos-de-dados/script.js
@@ -0,0 +1,21 @@
+// tipos de dados
+// Declare uma variável contendo uma string
+var str = "teste"
+
+// Declare uma variável contendo um número dentro de uma string
+var str2 = '30'
+
+// Declare uma variável com a sua idade
+var idade = 30
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var nome = "Luiz Ricardo"
+var sobrenome = "Silva"
+var nomeCompleto = nome + ' ' + sobrenome
+
+// Coloque a seguinte frase em uma variável: It's time
+var frase = "It's time"
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome)
---
+++ js-iniciantes/variaveis/script.js
@@ -0,0 +1,8 @@
+// variáveis
+// var nome = 'Luiz Ricardo Silva';
+// var idade = 30;
+// let comida;
+// comida = 'sushi';
+// let teste1, teste2, teste3, teste4, teste5;
+
+// console.log(nome, idade, comida, teste1, teste2, teste3, teste4, teste5)
