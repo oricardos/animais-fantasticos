28df1c279f2ca7a8118a565ec1f1f68a6c91704a
oricardos <oricardo.dev@gmail.com>
2024-08-23 22:19:38 -0300

    boolean e condicionais

---
+++ js-iniciantes/boolean&condicionais/script.js
@@ -0,0 +1,43 @@
+// Verifique se a sua idade é maior do que a de algum parente
+// Dependendo do resultado coloque no console 'É maior', 'É igual' ou 'É menor'
+const myAge = 30;
+const AliceAge = 31;
+
+if(myAge > AliceAge){
+    console.log('é maior')
+} else if(myAge === AliceAge){
+    console.log('é igual')
+} else {
+    console.log('é menor')
+}
+
+// Qual valor é retornado na seguinte expressão?
+var expressao = (5 - 2) && (5 - ' ') && (5 - 2);
+console.log(expressao)
+
+// Verifique se as seguintes variáveis são Truthy ou Falsy
+var nome = 'Andre';
+var idade = 28;
+var possuiDoutorado = false;
+var empregoFuturo;
+var dinheiroNaConta = 0;
+
+console.table(!!nome, !!idade, !!possuiDoutorado, !!empregoFuturo, !!dinheiroNaConta)
+
+// Compare o total de habitantes do Brasil com China (valor em milhões)
+var brasil = 207;
+var china = 1340;
+
+// O que irá aparecer no console?
+if(('Gato' === 'gato') && (5 > 2)) {
+  console.log('Verdadeiro');
+} else {
+  console.log('Falso');
+}
+
+// O que irá aparecer no console?
+if(('Gato' === 'gato') || (5 > 2)) {
+  console.log('Gato' && 'Cão');
+} else {
+  console.log('Falso');
+}
