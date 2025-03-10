9cf0683279e7f89cc2c917dcab0a8a976d32cb35
oricardos <oricardo.dev@gmail.com>
2025-03-10 15:16:25 -0300

    variaveis

---
+++ javascript-para-iniciantes/variaveis/script.js
@@ -0,0 +1,28 @@
+// Declarar uma variável com o seu nome
+var nome = 'Ricardo';
+
+// Declarar uma variável com a sua idade
+var idade = 31;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+
+let comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+
+comidaFavorita = 'seila, tanta coisa';
+
+// Declarar 5 variáveis diferentes sem valores
+let withoutValue1, withoutValue2, withoutValue3, withoutValue4, withoutValue5;
+
+console.table(
+  nome,
+  idade,
+  comidaFavorita,
+  withoutValue1,
+  withoutValue2,
+  withoutValue3,
+  withoutValue4,
+  withoutValue5
+);
