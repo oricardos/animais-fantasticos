a59e137c68e3db0b0179aa24ec507029ae09acde
oricardos <oricardo.dev@gmail.com>
2025-03-11 15:47:40 -0300

    tudo e objeto

---
+++ javascript-para-iniciantes/tudo-e-objeto/script.js
@@ -0,0 +1,9 @@
+// nomeie 3 propriedades ou métodos de strings
+// length / toLowerCase / toUpperCase
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+//addEventListener / getElementById / querySelector / querySelectorAll / getElementByClassname
+
+// busque na web um objeto (método) capaz de interagir com o clipboard,
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
+//writeText() e write()
