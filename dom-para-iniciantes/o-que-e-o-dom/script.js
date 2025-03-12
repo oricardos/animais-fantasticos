4970e1c47f8a6e4389e69d2aa3a2583a996da3ae
oricardos <oricardo.dev@gmail.com>
2025-03-12 10:49:36 -0300

    html do projeto

---
+++ dom-para-iniciantes/o-que-e-o-dom/script.js
@@ -0,0 +1,12 @@
+// Retorne o url da página atual utilizando o objeto window
+console.log(window.location.href);
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const ativo = document.querySelector('.ativo');
+console.log(ativo.innerHTML);
+
+// Retorne a linguagem do navegador
+console.log(navigator.language);
+// Retorne a largura da janela
+console.log(window.innerWidth);
