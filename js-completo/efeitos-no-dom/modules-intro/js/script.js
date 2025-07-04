9bb182912f9f79fe1ce61b591266f0e32d78ae72
oricardos <oricardo.dev@gmail.com>
2025-07-04 18:07:29 -0300

    modules

---
+++ js-completo/efeitos-no-dom/modules-intro/js/script.js
@@ -0,0 +1,9 @@
+import initSmoothScroll from './modules/scroll-suave.js';
+import initiAnimateScroll from './modules/initiAnimateScroll.js';
+import initAccordion from './modules/initAccordion.js';
+import initTabNav from './modules/initTabNav.js';
+
+initSmoothScroll();
+initiAnimateScroll();
+initAccordion();
+initTabNav();
