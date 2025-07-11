2739bd2cd8ec551c39a97dcd68f7d33cabd30022
oricardos <oricardo.dev@gmail.com>
2025-07-11 16:24:04 -0300

    tooltip

---
+++ js-completo/efeitos-no-dom/tooltip/js/script.js
@@ -0,0 +1,13 @@
+// import initSmoothScroll from './modules/scroll-suave.js';
+import initiAnimateScroll from './modules/initiAnimateScroll.js';
+import initAccordion from './modules/initAccordion.js';
+import initTabNav from './modules/initTabNav.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+
+// initSmoothScroll();
+initiAnimateScroll();
+initAccordion();
+initTabNav();
+initModal();
+initTooltip();
