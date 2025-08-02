3ef38b2291b79d5c0fbdfb7192d347abf4b9ddf8
oricardos <ricardo.rs95@gmail.com>
2025-08-02 17:57:31 -0300

    refatorando animascroll

---
+++ projeto-final/js/modules/animateScroll.js
@@ -0,0 +1,26 @@
+export default class AnimateScroll {
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
+    this.halfScreen = window.innerHeight * 0.5;
+
+    this.animateScroll = this.animateScroll.bind(this);
+  }
+
+  animateScroll() {
+    this.sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = sectionTop - this.halfScreen < 0;
+
+      if (isSectionVisible) {
+        section.classList.add('ativo');
+      } else if (section.classList.contains('ativo')) {
+        section.classList.remove('ativo');
+      }
+    });
+  }
+
+  init() {
+    this.animateScroll();
+    window.addEventListener('scroll', this.animateScroll);
+  }
+}
